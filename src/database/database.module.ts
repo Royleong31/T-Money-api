8d5fdccefba16b4034a30d5ff123ad656a304e80
Roy Leong <l3ongroy@gmail.com>
2023-09-01 21:02:44 +0800

    add db module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from 'db-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseProviders } from 'db-config';
+import { BusinessInfo } from 'src/entities/business-info.entity';
+import { User } from 'src/entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([BusinessInfo, BusinessInfo, User]),
+  ],
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
